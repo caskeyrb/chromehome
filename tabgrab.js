2ed069621087b6ad51555079f2c09d63a42b9ca1
miguelmejiamontes <miguelmejiamontes@comp1.local>
2015-02-07 15:00:05 -0500

    Implement launching of all 4 links at a time

--- tabgrab.js
+++ tabgrab.js
@@ -1,25 +1,26 @@
 var socialmedia = document.getElementById("socialmedialabel");
 socialmedia.onclick = function() {
-	alert("This would launch the social media links");
+	//alert("This would launch the social media links");
+	chrome.tabs.create({url: 'http://www.facebook.com'});
+	chrome.tabs.create({url: 'http://www.twitter.com'});
+	chrome.tabs.create({url: 'http://www.tumblr.com'});
+	chrome.tabs.create({url: 'http://www.instagram.com'});
 }
 
 var music = document.getElementById("musiclabel");
-
-//var socialmediabox = {1: "http://www.facebook.com", 2: "http://www.twitter.com", 3: "http://www.tumblr.com", 4: "http://www.instagram.com"};
-
-/*function linkLaunch(){
-	for(var i = 0; i < box.length; i++){
-		social
-	}
-		
-}
-*/
-
 music.onclick = function() {
-	alert("This would launch the music links");
+	//alert("This would launch the music links");
+	chrome.tabs.create({url: 'http://www.pandora.com'});
+	chrome.tabs.create({url: 'http://www.beatsmusic.com'});
+	chrome.tabs.create({url: 'http://www.soundcloud.com'});
+	chrome.tabs.create({url: 'http://www.iheartradio.com'});
 };
 
 var productivity = document.getElementById("productivitylabel");
 productivity.onclick = function() {
-	alert("This would launch the productivity links");
+	//alert("This would launch the productivity links");
+	chrome.tabs.create({url: 'http://www.linkedin.com'});
+	chrome.tabs.create({url: 'http://www.dropbox.com'});
+	chrome.tabs.create({url: 'http://www.github.com'});
+	chrome.tabs.create({url: 'http://www.google.com/drive/'});
 }
